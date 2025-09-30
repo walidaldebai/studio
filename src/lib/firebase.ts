98e748ece91c1dde03195d8d2b8ddc5bbd85ef8c
Walid Al Debai <walidaldebai@gmail.com>
2025-09-30 11:09:56 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
-// IMPORTANT: Replace this with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "TODO",
-  authDomain: "TODO",
-  projectId: "TODO",
-  storageBucket: "TODO",
-  messagingSenderId: "TODO",
-  appId: "TODO"
+  projectId: 'studio-8438523234-c4061',
+  appId: '1:992007136057:web:baa81002a224c229e3bee8',
+  apiKey: 'AIzaSyDXFrSq5NeGdxUsAU2B4c16ZA1qVSBO0oQ',
+  authDomain: 'studio-8438523234-c4061.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '992007136057',
 };
 
 // Initialize Firebase
