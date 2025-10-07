df1cb8236ed9ad99f5a9832e4f86cf9fa3532fdd
Walid Al Debai <walidaldebai@gmail.com>
2025-10-07 16:30:01 +0000

    make the ai work witrh frotnend and be able to communicate with the gemi

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {getApiKey} from '@/lib/server-utils';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: getApiKey('GEMINI_API_KEY')})],
   model: 'googleai/gemini-2.5-flash',
 });
